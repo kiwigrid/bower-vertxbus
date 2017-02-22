61259495886331e9149dfca57326cdeb69844287
Johannes Klein <johannes.klein@kiwigrid.com>
2017-02-22 15:57:18 +0100

    * code clean up

--- vertxbus.js
+++ vertxbus.js
@@ -229,8 +229,9 @@ if (module) {
 		}
 
 		function makeUUID() {
-			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(a, b) {
-				return b = Math.random() * 16, (a === 'y' ? b & 3 | 8 : b | 0).toString(16);
+			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(a) {
+				var b = Math.random() * 16;
+				return (a === 'y' ? b & 3 | 8 : b | 0).toString(16);
 			});
 		}
 	};
